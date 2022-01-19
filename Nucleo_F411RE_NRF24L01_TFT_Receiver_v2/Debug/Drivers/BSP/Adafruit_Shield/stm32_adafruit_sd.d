3f1ffd70f96d92f022d79cee382d8035ffcb1071
JC-Toussaint <jean-christophe.toussaint@grenoble-inp.fr>
2022-01-19 13:42:13 +0100

    second commit

--- Nucleo_F411RE_NRF24L01_TFT_Receiver_v2/Debug/Drivers/BSP/Adafruit_Shield/stm32_adafruit_sd.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Adafruit_Shield/stm32_adafruit_sd.o: \
- ../Drivers/BSP/Adafruit_Shield/stm32_adafruit_sd.c \
- ../Drivers/BSP/Adafruit_Shield/stm32_adafruit_sd.h
-
-../Drivers/BSP/Adafruit_Shield/stm32_adafruit_sd.h:
--- Nucleo_L432KC_NRF24L01_Transmitter_v1/Debug/Startup/startup_stm32l432kcux.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32l432kcux.o: ../Startup/startup_stm32l432kcux.s
